3682f6551a9a211c8c637a35b9b1f0b697e6fc14
Mego <mega.gamer05@gmail.com>
2018-05-28 13:56:26 -0500

    unpossible mode

---
+++ prog_unpossible.py
@@ -0,0 +1,2 @@
+lambda i, x: b"Hello,"[i] if i < len(b"Hello,") else x
+lambda i, x: b" World!\n"[i-6] if i >= 6 else None
--- run.py
+++ run.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 from ibmf import IBMF
+import argparse
+
+parser = argparse.ArgumentParser(description='IBMF')
+parser.add_argument("-u", "--unpossible", action='store_true', help='only one-line lambdas allowed as filters')
+args = parser.parse_args()
+
+if args.unpossible:
+    try:
+        with open('prog_unpossible.py') as prog:
+            filters = [eval(line, {}, {}) for line in prog]
+    except IOError:
+        print("Unable to open prog_unpossible.py, exiting!")
+else:
+    try:
+        import prog
+        filters = prog.filters
+    except ImportError:
+        print("Unable to open prog.py, exiting!")
+        exit(1)
 
 try:
-    import prog
-except ImportError:
-    prog = None
-    print("Unable to open prog.py, exiting!")
-    exit(1)
-
-try:
-    p = IBMF().add_filters(prog.filters)
+    p = IBMF().add_filters(filters)
 except AttributeError:
     print("Unable to load filters from prog.py, exiting!")
     exit(2)
